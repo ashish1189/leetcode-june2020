bf9b40400a590504e322a738537cd1dfe4b393e7
Ashish Deshpande <ashishdeshpande123@gmail.com>
2020-06-03 00:44:15 +0530

    2nd June 2020 solution

---
+++ src/june/two/DeleteLLNode.java
@@ -0,0 +1,15 @@
+package june.two;
+
+public class DeleteLLNode {
+	
+	private class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+}
